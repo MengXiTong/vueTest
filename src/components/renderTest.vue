96f4e773c301c3ba79c2ba0ebae9e07731f156cb
MengXiTong <1317545539@qq.com>
2019-01-02 16:11:54 +0800

    修改

---
+++ src/components/renderTest.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  render: function(createElement) {
+    return createElement(this.tag);
+  },
+  props: {
+    tag: {
+      type: String,
+      default: () => "p",
+      required: true
+    }
+  }
+};
+</script>
--- src/pages/jqTest.vue
+++ src/pages/jqTest.vue
@@ -1,26 +1,35 @@
 <template>
-    <div>
-        <h2 id="el" style="display:none;">Essential Links</h2>
-        <h3 id="hID">大家好我是测试测试</h3>
-        <form id="register">
-            <label for="password">Password:</label>
-            <input class="password" name="password" id="password" />
-            <div class="password-meter">
-                <div class="password-meter-message"> </div>
-                <div class="password-meter-bg">
-                    <div class="password-meter-bar"></div>
-                </div>
-            </div>
-            <input v-validate="'required|email'" :class="{'input': true, 'is-danger': errors.has('email') }" name="email" type="text" placeholder="Email">
-            <span v-show="errors.has('email')" class="help is-danger">{{ errors.first('email') }}</span>
-        </form>
-        <div id="btn-clean">清空</div>
-        <div class="local-value"></div>
-    </div>
+  <div>
+    <h2 id="el" style="display:none;">Essential Links</h2>
+    <h3 id="hID">大家好我是测试测试</h3>
+    <form id="register">
+      <label for="password">Password:</label>
+      <input class="password" name="password" id="password" />
+      <div class="password-meter">
+        <div class="password-meter-message"> </div>
+        <div class="password-meter-bg">
+          <div class="password-meter-bar"></div>
+        </div>
+      </div>
+      <input v-validate="'required|email'" :class="{'input': true, 'is-danger': errors.has('email') }" name="email" type="text" placeholder="Email">
+      <span v-show="errors.has('email')" class="help is-danger">{{ errors.first('email') }}</span>
+    </form>
+    <div id="btn-clean">清空</div>
+    <div class="local-value"></div>
+    <render-test tag="div"></render-test>
+    <render-test tag="p"></render-test>
+    <render-test tag="span"></render-test>
+    <input @keydown.enter="search" />
+  </div>
 </template>
 
 <script>
+import renderTest from "@/components/renderTest";
+
 export default {
+  components: {
+    renderTest
+  },
   methods: {
     getBase64Image(img) {
       var canvas = document.createElement("canvas");
@@ -36,7 +45,7 @@ export default {
       var image = new Image(); //至关重要
       image.crossOrigin = "";
       image.src = img; //至关重要
-    //   var deferred = $.Deferred(); //jquery中的promise
+      //   var deferred = $.Deferred(); //jquery中的promise
       let _self = this;
       // image.onload = function() {
       //   deferred.resolve(_self.getBase64Image(image)); //将base64传给done上传处理
@@ -48,6 +57,9 @@ export default {
           resolve(_self.getBase64Image(image)); //将base64传给done上传处理
         };
       });
+    },
+    search() {
+      console.log("执行查询");
     }
   },
   created() {
@@ -67,16 +79,18 @@ export default {
         $(".local-value").html("");
       });
       console.log($(".local-value")[0].innerHTML);
-      _self.getCanvasBase64(
-        "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2700762404,3403418546&fm=26&gp=0.jpg"
-      ).then(
-        function(base64) {
-          console.log("方式二》》》》》》》》》", base64);
-        },
-        function(err) {
-          console.log(err);
-        }
-      );
+      _self
+        .getCanvasBase64(
+          "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2700762404,3403418546&fm=26&gp=0.jpg"
+        )
+        .then(
+          function(base64) {
+            // console.log("方式二》》》》》》》》》", base64);
+          },
+          function(err) {
+            console.log(err);
+          }
+        );
     });
   }
 };
