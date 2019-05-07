5c3d649fea568f1b3b469cc4ad317e0d5315af05
MengXiTong <1317545539@qq.com>
2019-05-07 10:25:02 +0800

    日常测试

--- src/pages/cssHtml.vue
+++ src/pages/cssHtml.vue
@@ -11,10 +11,10 @@
           </div>
         </div>
       </el-collapse-item>
-      <el-collapse-item title="反馈 Feedback"
+      <el-collapse-item title="echarts使用测试"
                         name="2">
-        <div>控制反馈：通过界面样式和交互动效让用户可以清晰的感知自己的操作；</div>
-        <div>页面反馈：操作后，通过页面元素的变化清晰地展现当前状态。</div>
+        <div id="echarts"
+             style="width: 600px;height:400px;"></div>
       </el-collapse-item>
       <el-collapse-item title="效率 Efficiency"
                         name="3">
@@ -37,6 +37,7 @@
 </template>
 
 <script>
+const echarts = require("echarts");
 export default {
   data() {
     return {
@@ -44,9 +45,227 @@ export default {
     };
   },
   components: {},
-  methods: {},
-  created() {},
-  mounted() {}
+  methods: {
+    genData(count) {
+      var legendData = [];
+      var seriesData = [];
+      var selected = {};
+      for (var i = 0; i < 50; i++) {
+        name =
+          Math.random() > 0.65
+            ? this.makeWord(4, 1) + "·" + this.makeWord(3, 0)
+            : this.makeWord(2, 1);
+        legendData.push(name);
+        seriesData.push({
+          name: name,
+          value: Math.round(Math.random() * 100000)
+        });
+        selected[name] = i < 6;
+      }
+
+      return {
+        legendData: legendData,
+        seriesData: seriesData,
+        selected: selected
+      };
+    },
+    makeWord(max, min) {
+      var nameList = [
+        "赵",
+        "钱",
+        "孙",
+        "李",
+        "周",
+        "吴",
+        "郑",
+        "王",
+        "冯",
+        "陈",
+        "褚",
+        "卫",
+        "蒋",
+        "沈",
+        "韩",
+        "杨",
+        "朱",
+        "秦",
+        "尤",
+        "许",
+        "何",
+        "吕",
+        "施",
+        "张",
+        "孔",
+        "曹",
+        "严",
+        "华",
+        "金",
+        "魏",
+        "陶",
+        "姜",
+        "戚",
+        "谢",
+        "邹",
+        "喻",
+        "柏",
+        "水",
+        "窦",
+        "章",
+        "云",
+        "苏",
+        "潘",
+        "葛",
+        "奚",
+        "范",
+        "彭",
+        "郎",
+        "鲁",
+        "韦",
+        "昌",
+        "马",
+        "苗",
+        "凤",
+        "花",
+        "方",
+        "俞",
+        "任",
+        "袁",
+        "柳",
+        "酆",
+        "鲍",
+        "史",
+        "唐",
+        "费",
+        "廉",
+        "岑",
+        "薛",
+        "雷",
+        "贺",
+        "倪",
+        "汤",
+        "滕",
+        "殷",
+        "罗",
+        "毕",
+        "郝",
+        "邬",
+        "安",
+        "常",
+        "乐",
+        "于",
+        "时",
+        "傅",
+        "皮",
+        "卞",
+        "齐",
+        "康",
+        "伍",
+        "余",
+        "元",
+        "卜",
+        "顾",
+        "孟",
+        "平",
+        "黄",
+        "和",
+        "穆",
+        "萧",
+        "尹",
+        "姚",
+        "邵",
+        "湛",
+        "汪",
+        "祁",
+        "毛",
+        "禹",
+        "狄",
+        "米",
+        "贝",
+        "明",
+        "臧",
+        "计",
+        "伏",
+        "成",
+        "戴",
+        "谈",
+        "宋",
+        "茅",
+        "庞",
+        "熊",
+        "纪",
+        "舒",
+        "屈",
+        "项",
+        "祝",
+        "董",
+        "梁",
+        "杜",
+        "阮",
+        "蓝",
+        "闵",
+        "席",
+        "季",
+        "麻",
+        "强",
+        "贾",
+        "路",
+        "娄",
+        "危"
+      ];
+      var nameLen = Math.ceil(Math.random() * max + min);
+      var name = [];
+      for (var i = 0; i < nameLen; i++) {
+        name.push(nameList[Math.round(Math.random() * nameList.length - 1)]);
+      }
+      return name.join("");
+    }
+  },
+  created() {}, // 在模板渲染成html或者模板编译进路由前调用created
+  mounted() {
+    let _self = this;
+    $(function() {}); // mounted已完成模板已经渲染或el对应html渲染后
+    let data = _self.genData(50);
+    let option = {
+      title: {
+        text: "同名数量统计",
+        subtext: "纯属虚构",
+        x: "center"
+      },
+      tooltip: {
+        trigger: "item",
+        formatter: "{a} <br/>{b} : {c} ({d}%)"
+      },
+      legend: {
+        type: "scroll",
+        orient: "vertical",
+        right: 10,
+        top: 20,
+        bottom: 20,
+        data: data.legendData,
+
+        selected: data.selected
+      },
+      series: [
+        {
+          name: "姓名",
+          type: "pie",
+          radius: "55%",
+          center: ["40%", "50%"],
+          data: data.seriesData,
+          itemStyle: {
+            emphasis: {
+              shadowBlur: 10,
+              shadowOffsetX: 0,
+              shadowColor: "rgba(0, 0, 0, 0.5)"
+            }
+          }
+        }
+      ]
+    };
+    console.log(option);
+    let chart = echarts.init(document.getElementById("echarts"));
+    chart.setOption(option);
+  }
 };
 </script>
 
@@ -65,7 +284,6 @@ export default {
 }
 
 .line {
-
   .rightAngle {
     height: 40px;
     width: 20px;
